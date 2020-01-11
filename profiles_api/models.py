c97838e7ff2620a557b29f2413a1d8d162c695b1
abhishekdhs <abhishek.patel913@gmail.com>
2020-01-12 01:26:17 +0530

    Added HelloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -39,7 +39,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    address = models.TextField()
+    #address = models.TextField()
 
     objects = UserProfileManager()
 
